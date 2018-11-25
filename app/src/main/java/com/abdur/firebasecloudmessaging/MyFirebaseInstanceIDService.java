d5c1ed7f3c4c55f0b39c7d2fc0e51f3fd7d970b0
Hafiz Abd <abdurrahman.hafiz@yahoo.com>
2018-11-25 09:45:09 +0700

    first commit

---
+++ app/src/main/java/com/abdur/firebasecloudmessaging/MyFirebaseInstanceIDService.java
@@ -0,0 +1,13 @@
+package com.abdur.firebasecloudmessaging;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+    public void onTokenRefresh(){
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("FirebaseToken", "Refreshed Token = " + refreshedToken);
+    }
+}
